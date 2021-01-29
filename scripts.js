0d8221497eace897da60037a4b5cceb28f8355f9
mitaksim1 <miriamt.simonnet@gmail.com>
2021-01-29 15:58:52 +0100

    Creating the model of the html's table

--- scripts.js
+++ scripts.js
@@ -16,6 +16,28 @@ const Modal = {
     }
   }
 
+// Para recuperar os valores da tabela que criamos no html, precisamos estoca-los num vetor, no nosso caso um vetor de objetos [{}]
+const transactions = [
+    {
+      id: 1,
+      description: 'Luz',
+      amount: -50000, /* quando lidamos com valores nao colocamos as cifras e virgulas, iremos formata-los depois */
+      date: '23/01/2021',
+    },
+    {
+      id: 2,
+      description: 'Website',
+      amount: 500000, 
+      date: '23/01/2021',
+    },
+    {
+      id: 3,
+      description: 'Internet',
+      amount: -20000, 
+      date: '23/01/2021',
+    },
+];
+
 // Esse sinal de = nao devemos falar igual, mas sim atribuindo para evitar confusoes com o sinal de = matematico que significa igualdade
 const Transaction = {
     // Quais funcionalidades eu quero pra este objeto?
